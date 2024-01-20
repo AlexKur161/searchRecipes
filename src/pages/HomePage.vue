6f70712f9e103a72c546ffb450b65d19d90f85d0
AlexKur <AlexKur-web@yandex.ru>
2024-01-20 15:16:52 +0300

    /

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,9 +1,34 @@
-<template>test</template>
+<template>
+  <div class="main-wrapper">
+    <q-btn @click="getFood">test</q-btn>
+  </div>
+</template>
 
-<script>
-import { defineComponent } from "vue";
+<script setup>
+import { api } from "boot/axios";
 
-export default defineComponent({
-  name: "HomePage",
-});
+function getFood() {
+  api
+    .get(
+      "https://api.edamam.com/api/recipes/v2?type=public&app_id=9133ecf7&app_key=%201dc00f739ac8dce83875c61eae475139"
+    )
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
+}
 </script>
+<style>
+.main-wrapper {
+  background-image: url("/img/backgroundMain.png");
+  height: 700px;
+  background-size: cover;
+}
+</style>
